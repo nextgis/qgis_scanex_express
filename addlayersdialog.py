b6c2043700e4471f734b4918dce3758675e60a2e
Alexander Bruy <alexander.bruy@gmail.com>
2012-08-31 14:16:05 +0300

    forgot to change module name

--- addlayersdialog.py
+++ addlayersdialog.py
@@ -120,7 +120,7 @@ class AddLayersDialog( QDialog, Ui_Dialog ):
     url = QString( "http://maps.kosmosnimki.ru/TileService.ashx/apikey%1" ).arg( apiKey )
     uri.setParam( "url", url  )
 
-    provider = wmsprovider2.WmsProvider( uri.encodedUri() )
+    provider = wmsprovider.WmsProvider( uri.encodedUri() )
 
     if not provider.supportedLayers():
       #self.showError( provider.error )
