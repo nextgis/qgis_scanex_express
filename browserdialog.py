50f0a837292caca76b6b75c17224d6fb8d3fe840
Alexander Bruy <alexander.bruy@gmail.com>
2013-01-09 17:50:24 +0400

    more api-key related fixes

--- browserdialog.py
+++ browserdialog.py
@@ -27,6 +27,7 @@
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from PyQt4.QtNetwork import *
 
 from qgis.core import *
 from qgis.gui import *
@@ -38,17 +39,51 @@ class BrowserDialog( QDialog, Ui_Dialog ):
     QDialog.__init__( self )
     self.setupUi( self )
 
-    self.webView.load( QUrl("http://my.kosmosnimki.ru/Account/LoginDialog?client_id=6472&redirect_uri=http%3A%2F%2Flocalhost%3A1760%2FSite%2FoAuth%2FoAuthCallback.ashx%3Fcallback%3Dhttp%3A%2F%2Flocalhost%2Fapi%2FoAuthCallback.html&scope=basic&state=XAYLTRT6&partnerID=3be2ac3e-22cf-466c-9dda-66d0ec107352", QUrl.StrictMode ) )
+    self.webView.loadFinished.connect(self.manageSignals)
+    self.webView.load( QUrl("http://my.kosmosnimki.ru/Account/LoginDialog?client_id=6472&redirect_uri=http%3A%2F%2Flocalhost%3A1760%2FSite%2FoAuth%2FoAuthCallback.ashx%3Fcallback%3Dhttp%3A%2F%2Flocalhost%2Fapi%2FoAuthCallback.html&scope=basic&state=XAYLTRT6&partnerID=3be2ac3e-22cf-466c-9dda-66d0ec107352", QUrl.StrictMode) )
 
-    self.webView.urlChanged.connect(self.processUrl)
+  def manageSignals(self, loaded):
+    if loaded:
+      self.webView.loadFinished.disconnect(self.manageSignals)
+      self.webView.urlChanged.connect(self.processUrl)
 
   def processUrl(self, url):
-    print "URL changed", unicode(url.toString())
+    print "URL CHANGED:\n", url.toString()
 
-    state = url.encodedQueryItemValue("state")
-    print "STATE", state
-    if state != "XAYLTRT6":
-      print "Error"
+    mystate = url.queryItemValue( "state" )
+    if mystate != "XAYLTRT6":
+      print "state error", mystate
       return
 
-    code = url.encodedQueryItemValue("code")
+    mycode = url.encodedQueryItemValue( "code" )
+    mycode = url.queryItemValue( "code" )
+    print "CODE:\n", mycode
+
+    baseUrl = QUrl("http://my.kosmosnimki.ru/oAuth/AccessToken?client_id=6472&client_secret=45e96f17-26eb-4696-83fb-c5678adf4dda&code=" + mycode)
+    request = QNetworkRequest( baseUrl )
+    self.reply = QgsNetworkAccessManager.instance().get( request )
+
+    self.reply.finished.connect( self.replyFinished )
+    while self.reply:
+      QCoreApplication.processEvents( QEventLoop.ExcludeUserInputEvents )
+
+  def replyFinished( self ):
+    if self.reply.error() == QNetworkReply.NoError:
+      print "reply ok"
+
+      redirect = self.reply.attribute( QNetworkRequest.RedirectionTargetAttribute )
+      if not redirect.isNull():
+        print "request redirected"
+        print redirect.toUrl().toString()
+
+        request = QNetworkRequest( redirect.toUrl() )
+        self.reply.deleteLater()
+        self.reply = QgsNetworkAccessManager.instance().get( request )
+        self.reply.finished.connect( self.replyFinished )
+        return
+
+      response = self.reply.readAll()
+      print response
+
+    self.reply.deleteLater()
+    self.reply = None
